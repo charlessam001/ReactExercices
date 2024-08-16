85bc13d6f8342d0366b589a44af976272bbbcd7f
charlessam001 <charlessam728@gmail.com>
2024-08-17 00:00:05 +0200

    Update App2.jsx

--- App2.jsx
+++ App2.jsx
@@ -1,3 +1,4 @@
+// a way to make condition in react especially for color 
 import { useState } from 'react'
 
 import'./App.css'
@@ -6,9 +7,9 @@ function App() {
   const [count, setCount] = useState(0)
   const age = 17
   const isGreen = true;
-  
+  // { age >= 18 ? <h1>OVER_AGE</h1> : <h1>UNDER_AGE</h1>} : is among conditions we observed in the code
   return (
-      <div className="App">{ age >= 18 ? <h1>OVER_AGE</h1> : <h1 style={{color: isGreen ? "green" : "black"}}>UNDER_AGE</h1>}
+      <div className="App">{ age >= 18 ? <h1>OVER_AGE</h1> : <h1>UNDER_AGE</h1>}
       <h1 style={{color: isGreen ? "green" : "red"}}> THIS HAVE COLOR </h1>
       </div> )
       
